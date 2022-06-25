2d83857118e816f4299e440a1e04595bc6a8f4f2
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-25 18:12:37 +0200

    - [ Shared ] - Renamed folder Prova1 in Inscript

---
+++ Inscript/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inscript
+{
+    public class App
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine($"Hello World {50 + 51}!");
+        }
+
+    }
+}
