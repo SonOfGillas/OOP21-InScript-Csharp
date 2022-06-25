22946aaa5f666e309983e5a2437384f8d390b097
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-25 18:04:05 +0200

    - [ Shared ] - Initialization

---
+++ Prova1/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prova1
+{
+    public class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World {100 + 1}!");
+        }
+    }
+}
