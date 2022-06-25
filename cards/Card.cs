d99441968d58527e20184f3f82885115dd4edfe8
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-25 19:56:01 +0200

    - [ DrawPhase ] - Added some class and interface for drawphasemanager

---
+++ cards/Card.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace cards
+{
+    public class Card
+    {
+
+    }
+}
--- drawphasemanager/DrawPhaseManager.cs
+++ drawphasemanager/DrawPhaseManager.cs
@@ -1,26 +1,49 @@
 ﻿using System;
+using shared;
 
 namespace drawphasemanager
 {
-    public interface IDrawPhaseManager
+    public interface IDrawPhaseManager : IPhaseManager
     {
+        public const int INITIAL_CARD_IN_THE_HAND = 3;
+        public const int NO_MORE_CARD = 0;
         void draw(bool isTheAITurn);
 
         void firstDraw();
 
-        /* <da completare appena è stata aggiunta
-         *  la classe Player> */
-        /* void drawWithoutMana(Player player); */
+        void drawWithoutMana(Player player);
     }
 
     public class DrawPhaseManagerImpl : IDrawPhaseManager
     {
+
+        private bool _isTheAITurn;
+
+        private readonly Player _player;
+        private readonly Player _playerAI;
+        private readonly Random _rng = new Random();
+
+        public DrawPhaseManagerImpl(Player player, Player playerAI)
+        {
+            _player = player;
+            _playerAI = playerAI;
+        }
         public void draw(bool isTheAITurn)
         {
 
         }
 
         public void firstDraw()
+        {
+
+        }
+
+        public void drawWithoutMana(Player player)
+        {
+            
+        }
+
+        public void HandleEffect()
         {
 
         }
