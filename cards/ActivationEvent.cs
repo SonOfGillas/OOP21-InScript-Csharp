49ce373f4302a1f75c3240ad98bb41902463a23f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-28 14:15:09 +0200

    - [ Cards ] - Created ActivationEvent enum

---
+++ cards/ActivationEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cards
+{
+    public enum ActivationEvent
+    {
+        ENEMYDRAW,
+        MYDRAW,
+        EVERYDRAW,
+        POSITIONING,
+        ONATTAKING,
+        ONDEFENDING,
+        ONDEATH
+    }
+}
