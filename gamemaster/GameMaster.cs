c2498958aa3a0f3e28f2a199cc738739c2423856
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-28 18:01:08 +0200

    - [ GameMaster ] - Added an Interface

---
+++ gamemaster/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace gamemaster
+{
+
+    public interface IGameMaster
+    {
+        public static int DEFAULT_PLAYER_LIFE = 0;
+        public static int MIN_PLAYER_LIFE = -10;
+        public static int INITIAL_MANA = 0;
+        public static int MAXIMUM_MANA = 10;
+        public static int MANA_PLUS_ONE = 1;
+
+        /**
+         * this is the number of card that each player have in the hand before the drawingPhase
+         */
+        public static int INTIAL_NUM_CARDS_IN_HAND = 3;
+    }
+    class GameMaster
+    {
+
+    }
+}
