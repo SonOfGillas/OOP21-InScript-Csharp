15006fec7ae071681ee4fad2773751fa394f6e7f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-06-28 14:16:34 +0200

    - [ DrawPhase ] - Implemented some methods on DrawPhaseManagerImpl class

--- drawphasemanager/DrawPhaseManager.cs
+++ drawphasemanager/DrawPhaseManager.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using shared;
+using cards;
+
+using System.Collections.Generic;
+using System.Linq;
 
 namespace drawphasemanager
 {
@@ -19,8 +23,8 @@ namespace drawphasemanager
 
         private bool _isTheAITurn;
 
-        private readonly Player _player;
-        private readonly Player _playerAI;
+        private static Player _player;
+        private static Player _playerAI;
         private readonly Random _rng = new Random();
 
         public DrawPhaseManagerImpl(Player player, Player playerAI)
@@ -28,12 +32,42 @@ namespace drawphasemanager
             _player = player;
             _playerAI = playerAI;
         }
-        public void draw(bool isTheAITurn)
+
+        public void firstDraw()
         {
+
+            IEnumerable<int> range = Enumerable.Range(0, IDrawPhaseManager.INITIAL_CARD_IN_THE_HAND);
+
+            foreach(var elem in range)
+            {
+                generalDraw(_player);
+                generalDraw(_playerAI);
+            }
 
         }
 
-        public void firstDraw()
+        private void generalDraw(Player player)
+        {
+            IList<Card> tmpDeck = player.Deck;
+            IList<Card> tmpHand = player.Hand;
+
+            if (tmpDeck.Count > IDrawPhaseManager.NO_MORE_CARD)
+            {
+                int randInt = _rng.Next() % (tmpDeck.Count);
+                int index = Math.Abs(randInt);
+                int i = 0;
+
+                IEnumerator<Card> iterCard = tmpDeck.GetEnumerator();
+                for (;i < index; i++)
+                {
+                    iterCard.MoveNext();
+                }
+                tmpHand.Add(iterCard.Current);
+                tmpDeck.Remove(iterCard.Current);
+            }
+        }
+
+        public void draw(bool isTheAITurn)
         {
 
         }
