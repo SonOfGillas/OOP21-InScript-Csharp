5182c3aa1ee68a3fad70bdc8753c0224afdc94ea
riccardo gardenghi <riccardo.gardenghi@studio.unibo.it>
2022-06-29 19:02:59 +0200

    - [ Gamemaster ]  - init game master controller

---
+++ gamemaster/GameMasterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamemaster
+{
+   public interface IGameMasterController
+    {
+        public void onCardPlacing(int indexOfTheCellInTheBoard);
+        public void onEndTurn();
+    }
+
+    public class GameMasterController : IGameMasterController
+    {
+
+        public GameMasterController()
+        {
+
+        }
+
+        public void onCardPlacing(int indexOfTheCellInTheBoard)
+        {
+
+        }
+
+        public void onEndTurn()
+        {
+
+        }
+    }
+
+}
