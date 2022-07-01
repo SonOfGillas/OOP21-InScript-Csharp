e82475ae55ad24037d71c1c5bd831b16d1444354
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-07-01 17:51:47 +0200

    - [ TEST ] - Fixed BUG of Tests classes

---
+++ Test/DrawPhaseManagerTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gamemaster;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private IGameMaster _gameMaster;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
