93f5f1c0d9321ba2e452bc704cd5d2a003a7e92f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-07-01 19:12:10 +0200

    - [ TEST ] - Created new test class for DrawPhaseManagerImpl

--- Test/DrawPhaseManagerTests.cs
+++ Test/DrawPhaseManagerTests.cs
@@ -1,18 +1,45 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using gamemaster;
+using drawphasemanager;
+using cards;
+using shared;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace test
 {
     [TestClass]
-    public class UnitTest1
+    public class DrawPhaseManagerTests
     {
 
-        private IGameMaster _gameMaster;
+        private GameMaster _gameMaster;
+        private IDrawPhaseManager _drawPhase;
+        private Player _humanPlayer;
+        private Player _aiPlayer;
 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            List<Card> deck = new List<Card>();
+            IEnumerable<int> range = Enumerable.Range(0, 20);
+
+            foreach (int i in range)
+            {
+                deck.Add(new Card());
+            }
+
+            _gameMaster = new GameMaster(deck, deck);
+            _drawPhase = _gameMaster.DrawPhaseManager;
+            _humanPlayer = _gameMaster.HumanPlayer;
+            _aiPlayer = _gameMaster.AiPlayer;
+
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void FirstDraw()
         {
+            Assert.AreEqual(19, _humanPlayer.Deck.Count);
         }
     }
 }
