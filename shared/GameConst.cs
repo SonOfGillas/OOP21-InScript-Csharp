122afd9ed659683877a7deb63427befb3789e57f
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-07-02 11:01:13 +0200

    - [ Shared ] - Created IEffect interface

--- shared/GameConst.cs
+++ shared/GameConst.cs
@@ -7,6 +7,7 @@
         public static readonly int InitialMana = 0;
         public static readonly int MaximumMana = 10;
         public static readonly int ManaPlusOne = 1;
+        public static int MaximumUseEffect = 1;
 
         /**
          * this is the number of card that each player have in the hand before the drawingPhase
---
+++ shared/IEffect.cs
@@ -0,0 +1,18 @@
+﻿using cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shared
+{
+    public interface IEffect
+    {
+        
+        ActivationEvent ActivationEvent { get; }
+        string NameEffect { get; }
+        string DescriptionEffect { get; }
+        string ImageEffectURL { get; set; }
+        void UseEffect(Player cardOwner, Player enemy, int boardPosition);
+
+    }
+}
