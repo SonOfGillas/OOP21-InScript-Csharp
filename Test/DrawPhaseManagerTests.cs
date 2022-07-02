4c558c51d0f551aae9bb63360c7fda8d06bbedb7
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-07-02 11:39:29 +0200

    - [ TEST ] - Created all Tests for DrawPhaseManagerImpl class

--- Test/DrawPhaseManagerTests.cs
+++ Test/DrawPhaseManagerTests.cs
@@ -1,6 +1,5 @@
 using gamemaster;
 using drawphasemanager;
-using cards;
 using shared;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,6 +11,8 @@ namespace test
     [TestClass]
     public class DrawPhaseManagerTests
     {
+        public static bool AiTurn = true;
+        public static bool PlayerTurn = false;
 
         private GameMaster _gameMaster;
         private IDrawPhaseManager _drawPhase;
@@ -40,6 +41,63 @@ namespace test
         public void FirstDraw()
         {
             Assert.AreEqual(20, _humanPlayer.Deck.Count);
+            Assert.AreEqual(20, _aiPlayer.Deck.Count);
+
+            _drawPhase.FirstDraw();
+
+            Assert.AreEqual(17, _humanPlayer.Deck.Count);
+            Assert.AreEqual(17, _aiPlayer.Deck.Count);
+        }
+
+        [TestMethod]
+        public void NormalDraw()
+        {
+            Assert.AreEqual(20, _humanPlayer.Deck.Count);
+            Assert.AreEqual(20, _aiPlayer.Deck.Count);
+
+            _drawPhase.Draw(DrawPhaseManagerTests.AiTurn);
+            Assert.AreEqual(19, _aiPlayer);
+            Assert.AreEqual(1, _aiPlayer.Hand.Count);
+
+            _drawPhase.Draw(DrawPhaseManagerTests.PlayerTurn);
+            Assert.AreEqual(19, _humanPlayer);
+            Assert.AreEqual(1, _humanPlayer.Hand.Count);
+        }
+
+        [TestMethod]
+        public void ManaRestored()
+        {
+            _drawPhase.FirstDraw();
+
+            Assert.AreEqual(0, _humanPlayer.Mana);
+            Assert.AreEqual(0, _aiPlayer.Mana);
+
+            _drawPhase.Draw(DrawPhaseManagerTests.AiTurn);
+            _drawPhase.Draw(DrawPhaseManagerTests.PlayerTurn);
+            Assert.AreEqual(1, _aiPlayer.Mana);
+            Assert.AreEqual(1, _humanPlayer.Mana);
+
+            _drawPhase.Draw(DrawPhaseManagerTests.AiTurn);
+            _drawPhase.Draw(DrawPhaseManagerTests.PlayerTurn);
+            Assert.AreEqual(2, _aiPlayer.Mana);
+            Assert.AreEqual(2, _humanPlayer.Mana);
+        }
+
+        [TestMethod]
+        public void DrawWithoutMana()
+        {
+            _drawPhase.DrawWithoutMana(_aiPlayer);
+            Assert.AreEqual(0, _aiPlayer.Mana);
+
+            _drawPhase.DrawWithoutMana(_humanPlayer);
+            Assert.AreEqual(0, _humanPlayer.Mana);
+
+        }
+
+        [TestMethod]
+        public void PlaceaDrawEffectCard()
+        {
+            // TODO
         }
     }
 }
