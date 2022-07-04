ac798fc8de2ae9203f88511f5f7d89ad6f8a025b
michele.andrenacci@studio.unibo.it <michele.andrenacci@studio.unibo.it>
2022-07-04 09:31:46 +0200

    -[Card]- Add Interface ICard

---
+++ shared/ICard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace shared
+{
+    public interface ICard
+    {
+        public string IdCard { get; }
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Mana { get; set; }
+        public int PlacementRounds { get; set; }
+        public int LifePoints { get; set; }
+        public IEffect Effect { get; set; }
+        public string ImageURL { get; set; }
+
+       
+    }
+}
